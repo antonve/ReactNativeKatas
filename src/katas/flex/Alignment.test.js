b4cf75fcbf9c8aaee2e124ce4cb2dc2938f41366
Anton Van Eechaute <anton@wantedly.com>
2018-01-21 11:54:25 +0900

    Solve alignment

--- src/katas/flex/Alignment.test.js
+++ src/katas/flex/Alignment.test.js
@@ -1,15 +1,8 @@
-
-import React, { Component } from 'react';
-import ReactNative, {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'
-import colors from '@/runner/colors'
-import Box from './box'
-
+import React, { Component } from "react";
+import ReactNative, { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import colors from "@/runner/colors";
+import Box from "./box";
 
 // Flex Alignment
 //
@@ -21,38 +14,36 @@ import Box from './box'
 //         'flexbox alignment'
 //
 
-const Alignment= (props)=>{
+const Alignment = props => {
   return (
     <View style={styles.container}>
-      <Box/>
-      <Box/>
-      <Box/>
-      <Box/>
-      <Box/>
-      <Box/>
-      <Box/>
+      <Box />
+      <View style={{ alignItems: "center" }}>
+        <Box />
+        <View style={{ flexDirection: "row" }}>
+          <Box />
+          <Box />
+          <Box />
+        </View>
+        <Box />
+      </View>
+      <Box />
     </View>
-  )
-}
-
+  );
+};
 
 // TODO: A focusing instruction, what's the immediate thing I need to do?
 //
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors[1],
-    flex:1,
+    flex: 1,
   },
-  text:{
-    color: 'white',
-    textAlign:'center'
-  }
+  text: {
+    color: "white",
+    textAlign: "center",
+  },
 });
 
-
-
-
-
-Alignment.displayName = 'Alignment'
-export default Alignment
-
+Alignment.displayName = "Alignment";
+export default Alignment;
