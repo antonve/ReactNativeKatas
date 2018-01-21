6e25fd00e42cce66930a77887277420bfb48e46a
Anton Van Eechaute <anton@wantedly.com>
2018-01-21 15:59:33 +0900

    Solve clipping

--- src/katas/styling/Clipping.test.js
+++ src/katas/styling/Clipping.test.js
@@ -1,15 +1,7 @@
-
-import React, { Component } from 'react';
-import ReactNative, {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'
-import colors from '@/runner/colors'
-
-
+import React, { Component } from "react";
+import ReactNative, { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import colors from "@/runner/colors";
 
 // Clipping
 //
@@ -21,33 +13,26 @@ import colors from '@/runner/colors'
 // is a notorious one.
 //
 
-const Clipping= (props)=>{
+const Clipping = props => {
   return (
     <View style={styles.container}>
-      <View style={{height:80, width:80, borderRadius:40}}>
-        <View style={{height:80, width:80, backgroundColor:'orange'}}>
-        </View>
+      <View style={{ height: 80, width: 80, borderRadius: 40, overflow: "hidden" }}>
+        <View style={{ height: 80, width: 80, backgroundColor: "orange" }} />
       </View>
     </View>
-  )
-}
-
+  );
+};
 
 // TODO: A focusing instruction, what's the immediate thing I need to do?
 //
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: colors[2],
   },
 });
 
-
-
-
-
-Clipping.displayName = 'Clipping'
-export default Clipping
-
+Clipping.displayName = "Clipping";
+export default Clipping;
