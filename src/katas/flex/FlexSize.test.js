e9305877999b0df7edc1a224ab042a3fbbc7e09a
Anton Van Eechaute <anton@wantedly.com>
2018-01-21 12:25:11 +0900

    Solve flexsize

--- src/katas/flex/FlexSize.test.js
+++ src/katas/flex/FlexSize.test.js
@@ -1,16 +1,8 @@
-
-import React, { Component } from 'react';
-import ReactNative, {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import Box from './box'
-import Icon from 'react-native-vector-icons/Ionicons'
-import colors from '@/runner/colors'
-
-
+import React, { Component } from "react";
+import ReactNative, { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Box from "./box";
+import Icon from "react-native-vector-icons/Ionicons";
+import colors from "@/runner/colors";
 
 // Flex Sizing
 //
@@ -22,38 +14,32 @@ import colors from '@/runner/colors'
 // Google: flex visual weight
 //
 
-const FlexSize= (props)=>{
+const FlexSize = props => {
   return (
     <View style={styles.container}>
-      <View style={{}}>
-        <Box style={{}}/>
-        <Box style={{backgroundColor:'yellow'}}/>
-        <Box/>
-        <Box style={{backgroundColor:'yellow'}}/>
+      <View style={{ flex: 1 }}>
+        <Box style={{ flex: 0.7 }} />
+        <Box style={{ backgroundColor: "yellow" }} />
+        <Box />
+        <Box style={{ backgroundColor: "yellow", flex: 0.3 }} />
       </View>
-      <View style={{}}>
-        <Box style={{flex:1}}/>
-        <Box style={{backgroundColor:'yellow'}}/>
-        <Box/>
-        <Box style={{backgroundColor:'yellow'}}/>
+      <View style={{ flex: 1 }}>
+        <Box style={{ flex: 1 }} />
+        <Box style={{ backgroundColor: "yellow" }} />
+        <Box />
+        <Box style={{ backgroundColor: "yellow", flex: 1 }} />
       </View>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    flexDirection:'row',
+    flex: 1,
+    flexDirection: "row",
     backgroundColor: colors[1],
   },
 });
 
-
-
-
-
-FlexSize.displayName = 'FlexSize'
-export default FlexSize
-
+FlexSize.displayName = "FlexSize";
+export default FlexSize;
