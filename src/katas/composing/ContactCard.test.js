6101af072e075293010a19f2ba162f57826ee751
Anton Van Eechaute <anton@wantedly.com>
2018-01-21 16:19:04 +0900

    Solve contact card

--- src/katas/composing/ContactCard.test.js
+++ src/katas/composing/ContactCard.test.js
@@ -1,15 +1,7 @@
-
-import React, { Component } from 'react';
-import ReactNative, {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'
-import colors from '@/runner/colors'
-
-
+import React, { Component } from "react";
+import ReactNative, { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import colors from "@/runner/colors";
 
 // Contact Card
 //
@@ -24,45 +16,51 @@ import colors from '@/runner/colors'
 // the shadow color you should use: #175E4C, with opacity 0.7, offsets 2(w), 6(h)
 //
 
-const ContactCard= (props)=>{
+const ContactCard = props => {
   return (
-    <View style={[styles.container, {alignItems:'stretch'}]}>
-      <View style={{}}>
-        <View style={{flexDirection:'row'}}>
-          <View style={{width:80, height:80, backgroundColor:'#229E85'}}>
-          </View>
-          <View style={{}}>
+    <View style={[styles.container, { alignItems: "stretch" }]}>
+      <View
+        style={{
+          backgroundColor: "#34B99A",
+          padding: 20,
+          margin: 10,
+          shadowColor: "#175E4C",
+          shadowOffset: { width: 2, height: 6 },
+          shadowOpacity: 0.7,
+          shadowRadius: 10,
+        }}>
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ width: 80, height: 80, backgroundColor: "#229E85", borderRadius: 40, marginBottom: 20 }} />
+          <View style={{ marginLeft: 20, marginTop: 10, alignItems: "flex-start" }}>
             <Text style={styles.text}>Chuck Norris</Text>
             <Text style={styles.text}>CEO</Text>
           </View>
         </View>
         <View style={{}}>
-          <Text style={{color:'white'}}><Icon name="ios-call-outline" size={16}/> 481-5162342</Text>
-          <Text style={{color:'white'}}><Icon name="ios-send-outline" size={16}/> chuck@norrisfamily.arpa</Text>
+          <Text style={{ color: "white" }}>
+            <Icon name="ios-call-outline" size={16} /> 481-5162342
+          </Text>
+          <Text style={{ color: "white" }}>
+            <Icon name="ios-send-outline" size={16} /> chuck@norrisfamily.arpa
+          </Text>
         </View>
       </View>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: colors[2],
   },
-  text:{
-    color: 'white',
-    textAlign:'center'
-  }
+  text: {
+    color: "white",
+    textAlign: "center",
+  },
 });
 
-
-
-
-
-ContactCard.displayName = 'ContactCard'
-export default ContactCard
-
+ContactCard.displayName = "ContactCard";
+export default ContactCard;
