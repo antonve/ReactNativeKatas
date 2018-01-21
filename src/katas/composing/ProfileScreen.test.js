38bb9b30d3e1be1896e09f964b8e6a131a45e122
Anton Van Eechaute <anton@wantedly.com>
2018-01-21 18:14:49 +0900

    Solve profilescreen

--- src/katas/composing/ProfileScreen.test.js
+++ src/katas/composing/ProfileScreen.test.js
@@ -1,22 +1,14 @@
+import React, { Component } from "react";
+import ReactNative, { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import colors from "@/runner/colors";
 
-import React, { Component } from 'react';
-import ReactNative, {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'
-import colors from '@/runner/colors'
-
-
-
-const settings =  {
-  "Name":"Chuck Norris",
-  "Address":"56th Lowkick Ave., Texas",
-  "Phone":"481-5162342",
-  "Email":"chuck@norrisfamily.arpa",
-}
+const settings = {
+  Name: "Chuck Norris",
+  Address: "56th Lowkick Ave., Texas",
+  Phone: "481-5162342",
+  Email: "chuck@norrisfamily.arpa",
+};
 
 // Profile Screen
 //
@@ -29,47 +21,42 @@ const settings =  {
 // - Components can alignSelf
 //
 
-const ProfileScreen = (props)=>{
+const ProfileScreen = props => {
   return (
     <View style={styles.container}>
-      <View style={{backgroundColor:'gray', width:120, height:120,}}>
-      </View>
-      <Text style={{fontSize:26,}}>
-        Chuck Norris
-      </Text>
-      {
-        Object.keys(settings).map((key)=>{
-          return <View key={key} style={styles.setting}>
-            <Text style={styles.settingProp}>
-              {key}
-            </Text>
-            <Text style={styles.settingVal}>
-              {settings[key]}
-            </Text>
+      <View style={{ backgroundColor: "gray", width: 120, height: 120, borderRadius: 60, marginTop: 30 }} />
+      <Text style={{ fontSize: 26, fontWeight: "100", marginTop: 20, marginBottom: 30 }}>Chuck Norris</Text>
+      {Object.keys(settings).map(key => {
+        return (
+          <View key={key} style={styles.setting}>
+            <Text style={styles.settingProp}>{key}</Text>
+            <Text style={styles.settingVal}>{settings[key]}</Text>
           </View>
-        })
-      }
-
+        );
+      })}
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  setting:{
+  setting: {
+    borderColor: "#d8d8d8",
+    borderBottomWidth: 1,
+    alignSelf: "stretch",
+    flexDirection: "row",
+    padding: 20,
   },
-  settingProp:{
-    color:'#888',
-    marginRight:10,
+  settingProp: {
+    color: "#888",
+    marginRight: 10,
+    width: 70,
   },
   container: {
-    flex:1,
-    backgroundColor: 'white',
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
   },
 });
 
-
-
-
-ProfileScreen.displayName = 'ProfileScreen'
-export default ProfileScreen
-
+ProfileScreen.displayName = "ProfileScreen";
+export default ProfileScreen;
